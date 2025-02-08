43ddf913f2233f3838e9c75a9fa343dca1348038
vibe-34 <vibe.34@gmail.com>
2025-02-08 11:06:51 +0300

    Замена БД с SQLite на PostgresSQL

--- project/project/settings.py
+++ project/project/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',                                    # Имя базы данных
+        'USER': 'postgres',                                    # Имя пользователя PostgreSQL
+        'PASSWORD': 'postgres',                                # Пароль для пользователя от БД
+        'HOST': 'localhost',                                   # Или IP-адрес сервера
+        'PORT': '',                                            # По умолчанию используется 5432
     }
 }
 
